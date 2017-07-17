9b480dd54c8a48178f0724d8eef42a761bba1711
hai1211 <dlhai1211@gmail.com>
2017-07-17 14:29:13 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
-  def new; end
+  private
+
+  attr_reader :user
+
+  public
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if user.save
+      flash[:success] = t "user.welcome"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    return if user
+    flash[:danger] = t "user.show.not_exist"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
