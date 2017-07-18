c572efd146e8c1960e1374c476ec26d8ccfa81a0
hai1211 <dlhai1211@gmail.com>
2017-07-18 17:39:59 +0700

    Basic Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+  private
+
+  attr_reader :user
+
+  public
+
+  def new; end
+
+  def create
+    session = params[:session]
+    @user = User.find_by email: session[:email].downcase
+
+    if user && user.authenticate(session[:password])
+      login_success user
+    else
+      login_fail
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def login_success user
+    log_in user
+    redirect_to user
+  end
+
+  def login_fail
+    flash[:danger] = t "error.login_failt"
+    render :new
+  end
+end
